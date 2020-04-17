7d2644ecb91797f586d88d6c5ad0b86a6ea3a2e8
Alexey Nikolaev <nikolaev.an87@gmail.com>
2020-04-17 23:16:02 +0000

    second commit

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from articles.models import Article
 
@@ -11,3 +11,34 @@ def get_article(request, article_id):
         return render(request, 'article.html', {"post": post})
     except Article.DoesNotExist:
         raise Http404
+
+def create_post(request):
+    if not request.user.is_anonymous:
+        if request.method == "POST":
+        # обработать данные формы, если метод POST
+            form = {
+                'text': request.POST["text"], 'title': request.POST["title"]
+            }
+        # в словаре form будет храниться информация, введенная пользователем
+            if form["text"] and form["title"] and uniq(form["title"]):
+        # если поля заполнены без ошибок
+                Article.objects.create(text=form["text"], title=form["title"], author=request.user)
+                return redirect('archive')
+            # перейти на страницу поста
+            else:
+        # если введенные данные некорректны
+                form['errors'] = u"Не все поля заполнены или корректны"
+                return render(request, 'create_post.html', {'form': form})
+        else:
+        # просто вернуть страницу с формой, если метод GET
+            return render(request, 'create_post.html', {})
+    else:
+        raise Http404
+
+def uniq(title):
+    posts = Article.objects.all()
+    for post in posts:
+        if title == post.title:
+            return False
+        else:
+            return True
--- blog/urls.py
+++ blog/urls.py
@@ -20,5 +20,6 @@ from articles import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.archive, name="archive"),
+    path('article/new/', views.create_post, name='create_post'),
     re_path(r'^article/(?P<article_id>[0-9]+)$', views.get_article, name='get_article'),
 ]
